6cf36f751ac0b8031204bd47e626131b094ccb93
Terry Yanko <terryyanko@gmail.com>
2016-05-18 23:05:45 -0400

    exposed default http port, tests green

--- spec/192.168.99.100/Dockerfile_spec.rb
+++ spec/192.168.99.100/Dockerfile_spec.rb
@@ -1,35 +1,41 @@
 # spec/Dockerfile_spec.rb
 
-require "serverspec"
-require "docker"
+require 'serverspec'
+require 'docker'
+require 'rspec'
 
-describe "Dockerfile" do
-  before(:all) do
-    image = Docker::Image.build_from_dir('.')
+describe 'Dockerfile' do
+    before(:all) do
+        image = Docker::Image.build_from_dir('.')
 
-    set :os, family: :debian
-    set :backend, :docker
-    set :docker_image, image.id
-  end
-	
-	context "Verifies that correct operating system is installed" do
-		it "installs the right version of Ubuntu" do
-			expect(os_version).to include("Ubuntu 14")
-		end
-  end
+        set :os, family: :debian
+        set :backend, :docker
+        set :docker_image, image.id
+    end
 
-	context "Verify correct packages are installed" do
-		it "install and run apache" do
-			expect(package("apache2")).to be_installed
-			expect(service("apache2")).to be_running 
-		end
+    context 'Verifies that correct operating system is installed' do
+        it 'installs the right version of Ubuntu' do
+            expect(os_version).to include('Ubuntu 14')
+        end
+    end
 
-	  it "install php5" do 
-	    expect(package("php5")).to be_installed
-	  end 
-	end
-  def os_version
-    command("lsb_release -a").stdout
-  end
+    context 'Verify correct packages are installed' do
+        it 'install and run apache' do
+            expect(package('apache2')).to be_installed
+            expect(service('apache2')).to be_running
+        end
+
+        it 'install php5' do
+            expect(package('php5')).to be_installed
+        end
+    end
+    def os_version
+        command('lsb_release -a').stdout
+    end
+
+    context 'Configure the default http port' do
+        it 'exposes port 80' do
+            expect(port(80)).to be_listening
+        end
+    end
 end
-	
